fefcff071706185b57ec3dd24bbc9a9a3185c99c
earba <earbajsaria3@gmail.com>
2023-09-07 15:30:30 +0600

    Add user class and user methode to auth class to get current user

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,22 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../models/user.dart';
+
 class Authentication{
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  // create user obj based on firebase user
+  Users? _userFromFirebaseUser(User user) {
+    return user != null ? Users(uid: user.uid) : null;
+  }
+
+  // sign in anon
   Future signInAno() async {
     try{
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
-      return user;
+      return _userFromFirebaseUser(user!);
     }catch(e){
       print(e.toString());
       return null;
