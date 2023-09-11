5f56bc062c237b1cac2023ade27ed5a1c9ffd6ce
earba <earbajsaria3@gmail.com>
2023-09-11 10:25:35 +0600

    Add register with gmail and password functionality

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -28,6 +28,19 @@ class Authentication{
       return null;
     }
   }
+
+  // register with email and password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      User? user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+
   // sign out
   Future signOut() async {
     try {
